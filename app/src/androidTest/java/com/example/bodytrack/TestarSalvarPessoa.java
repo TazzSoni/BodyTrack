40812a464d3933a4fb28cf356086f3531f9f83a1
Rodrigo <tazzsoni@gmail.com>
2021-08-18 19:30:45 -0300

    Classe de testes

---
+++ app/src/androidTest/java/com/example/bodytrack/TestarSalvarPessoa.java
@@ -0,0 +1,11 @@
+package com.example.bodytrack;
+
+import junit.framework.TestCase;
+
+public class TestarSalvarPessoa extends TestCase {
+
+    public void testarSalvarPessoa() {
+        
+    }
+
+}
